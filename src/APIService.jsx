0ccf41cad08989debdfad230c6a19f6c5544a65e
Ravina-Duggal <143420735+Ravina-Duggal@users.noreply.github.com>
2026-07-15 09:30:16 +0530

    Update APIService.jsx

--- src/APIService.jsx
+++ src/APIService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const BASE_URL = "http://192.168.1.6:8000/";
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "http://52.65.135.238:8000/";
 
 class APIService {
 
